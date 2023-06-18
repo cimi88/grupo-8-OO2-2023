882f5e85afe7a9654a5ff8d202336168c48cb90b
Cristian Santander <casdesarrollo.2022@gmail.com>
2023-06-17 22:04:26 -0300

    agregando home

---
+++ src/main/java/com/unla/grupo8/helpers/ViewRouteHelpers.java
@@ -0,0 +1,14 @@
+package com.unla.grupo8.helpers;
+
+public class ViewRouteHelpers {
+	
+	public final static String INDEX = "home/index";
+	
+	//HELPERS PARA DISPOSITIVOS
+	
+	//HELPERS USUARIO
+	
+	public final static String USER_LOGIN = "user/login";
+	public final static String USER_LOGOUT = "user/logout";
+
+} 
