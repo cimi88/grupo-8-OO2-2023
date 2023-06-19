84a4af0f57e841d519955c38de7c4e62f352607b
Cristian Santander <casdesarrollo.2022@gmail.com>
2023-06-18 22:45:12 -0300

    solucionando pero con error tambien :)

--- src/main/java/com/unla/grupo8/converters/DispositivoAlumbradoConverter.java
+++ src/main/java/com/unla/grupo8/converters/DispositivoAlumbradoConverter.java
@@ -1,13 +1,17 @@
 package com.unla.grupo8.converters;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.unla.grupo8.entities.DispositivoAlumbrado;
 import com.unla.grupo8.models.DispositivoAlumbradoModelo;
 
-@Component("personaConverter")
+@Component("dispositivoAlumbradoConverter")
 public class DispositivoAlumbradoConverter {
 	
+	@Autowired
+	@Qualifier("avatarConverter")
 	//por ahora se cargara sin las listas
 	public DispositivoAlumbradoModelo entityToModel(DispositivoAlumbrado dispoAlum) {
 		return new DispositivoAlumbradoModelo (dispoAlum.getId(), dispoAlum.getNombre(), dispoAlum.isEnAlta(), dispoAlum.isEncendido(), 
