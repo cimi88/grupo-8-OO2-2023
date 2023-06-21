377c1d2a34a10a163e7bf2adb9f02b0b778b03cb
Cristian Santander <casdesarrollo.2022@gmail.com>
2023-06-21 16:02:23 -0300

    tema consola solucionado

--- src/main/java/com/unla/grupo8/controllers/AlumbradoController.java
+++ src/main/java/com/unla/grupo8/controllers/AlumbradoController.java
@@ -1,33 +1,20 @@
 package com.unla.grupo8.controllers;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
-
-import com.example.ejemplos_video.models.PersonaModelo;
 import com.unla.grupo8.helpers.ViewRouteHelpers;
 import com.unla.grupo8.models.DispositivoAlumbradoModelo;
-import com.unla.grupo8.repositories.IEspacioRepository;
 import com.unla.grupo8.services.IDispositivoAlumbradoService;
-import com.unla.grupo8.services.IEspacioService;
 import com.unla.grupo8.services.implementations.EspacioService;
 
 import jakarta.validation.Valid;
@@ -99,8 +86,7 @@ public class AlumbradoController {
 		
 		return modelAndView;	
 	}
-	
-	
+
 	@GetMapping("/bajaAlumbrado/{id}")
 	public ModelAndView bajaDispositivoLucesAutomaticas (@PathVariable("id")int id) {
 		dispositivoAlumbradoService.baja(id);
